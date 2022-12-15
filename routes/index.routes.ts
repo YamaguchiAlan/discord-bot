47bf4a580736473df058ab0bb082d565fa83dfcd
YamaguchiAlan <alanyamaguchi72@gmail.com>
2022-12-15 04:28:29 -0300

    testing runner

--- routes/index.routes.ts
+++ routes/index.routes.ts
@@ -10,7 +10,7 @@ import { MessageType } from './types'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-  res.status(200).send({ ok: 'obvcio padre vamos la scaloneta' })
+  res.status(200).send({ ok: true })
 })
 
 router.post('/twitch/stream/live', async (req: Request, res: Response) => {
