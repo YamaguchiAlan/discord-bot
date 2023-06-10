9f899763d1978d25c6e03377ea2bb15c7cf2781e
YamaguchiAlan <alanyamaguchi72@gmail.com>
2023-06-09 21:54:32 -0300

    remove dist

--- dist/routes/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MessageType = void 0;
-/* eslint-disable no-unused-vars */
-var MessageType;
-(function (MessageType) {
-    MessageType["Notification"] = "notification";
-    MessageType["CallbackVerification"] = "webhook_callback_verification";
-})(MessageType || (exports.MessageType = MessageType = {}));
